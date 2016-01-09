60c69754ebe78328f07238aa09ee1252eb51ebed
ivandejanovic <idejanovic@gmail.com>
2016-01-09 12:08:44 +0100

    Added AJAX and backend http call examples.

--- modules/routes/index.js
+++ modules/routes/index.js
@@ -1,3 +1,5 @@
+http = require('http');
+
 module.exports = (function() {
   'use strict';
 
@@ -7,12 +9,36 @@ module.exports = (function() {
   
   routes.setRoutes = function(app, kursrs) {
     app.get('/', routes.home);
+    app.get('/ajax', routes.ajax);
     
     errors.setErrors(app);
   };
 
   routes.home = function(req, res) {
     res.render('home', {});
+  };
+  
+  routes.ajax = function(req, res) {
+    var options = {
+      host: 'www.google.com',
+      path: '/'
+    };
+    
+    http.request(options, function(response){
+      var data = '';
+
+      response.on('data', function(chunk) {
+        data += chunk;
+      });
+
+      response.on('end', function() {
+        res.send(data);
+      });
+
+      response.on('error', function(err) {
+        console.log('Problem with request: ' + err.message);
+      });
+    }).end();
   };
 
   return routes;
--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,4 +1,27 @@
-(function(root) {
+(function(root, document) {
   'use strict';
+  
+  var message = document.getElementById('message');
+  var button = document.getElementById('button');
+  
+  if (message && button) {
+    var clickHandler = function(event) {
+      var xhr = new XMLHttpRequest({mozSystem: true});
+      xhr.open("GET", "/ajax", true);
+      xhr.onreadystatechange = function () {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+          message.innerHTML = "Ajax succeded";
+        }
+      };
 
-}(window));
+      xhr.onerror = function () {
+        message.innerHTML = "Ajax failed.";
+      };
+      
+      xhr.send();
+    };
+
+    button.addEventListener('click', clickHandler);
+  }
+
+}(this, this.document));
